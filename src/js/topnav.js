f4825fe5180e9f5db9147fb033b8b7aefd55b3a6
Johnnyyu3 <jyy3@njit.edu>
2025-07-03 12:48:52 -0400

    Blueprints for pages + working topNav

--- src/js/topnav.js
+++ src/js/topnav.js
@@ -1,10 +1,18 @@
 // loadNav.js
+const activeId = document.currentScript.getAttribute('active');
+
+
 fetch('./src/html/topnav.html') // Fetch the topnav.html file
   .then(response => response.text()) // Get the response as plain text (HTML)
   .then(html => {
     // Insert the fetched HTML into the container div
     document.getElementById('topnav-container').innerHTML = html;
+    const activePage = document.getElementById(activeId);
+    activePage.setAttribute("class", "active");
+
   })
   .catch(error => { // Add error handling
     console.error('Error loading navigation:', error);
   });
+
+
